d84b26f9217a1309c82de7043c1358875a0f5cbf
mueiya <mueiya.whatever@gmail.com>
2024-01-17 02:54:54 +0700

    feat: add books storage

---
+++ src/books.js
@@ -0,0 +1,10 @@
+/**
+ * This file is temporary storage module for storing books,
+ * because can't use DB.
+ */
+
+// An array to temporarily store books
+const books = [];
+
+// Export the array
+module.exports = books;
