d234e3eb2979387cdbf6a5921e6aa874f2ca81e8
mueiya <mueiya.whatever@gmail.com>
2024-01-16 23:19:27 +0700

    feat: set up the server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+// Initialize an async function to set up the Hapi server
+const init = async () => {
+  // Create Hapi server instance
+  const server = Hapi.server({
+    // Set the port for the server
+    port: 9000,
+    // Set the host for the server
+    host: 'localhost',
+    routes: {
+      cors: {
+        // Enable CORS for all origins
+        origin: ['*'],
+      },
+    },
+  });
+
+  // Register routes with the server
+  server.route(routes);
+
+  // Start the server
+  await server.start();
+  // Print on server uri on console
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+init();
